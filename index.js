59cc33b09d3d46d68a9cb1f4baaf13e8fb75f591
Ethan Ding <ethanding@Ethans-MacBook-Pro.local>
2020-05-18 17:06:09 -0400

    got it working

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = require("express")()
 
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/index.html `)
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+app.get("/poopoo", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
 })
 
 app.listen(8080, () => console.log('hi I\'m listening'))
