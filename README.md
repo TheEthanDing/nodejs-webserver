e04bad51e6dca7cf07e2c7bc074b91f4ca342c04
Ethan Ding <ethanding@Ethans-MacBook-Pro.local>
2020-05-18 16:54:07 -0400

    removed etire node library

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nodejs-webserver
+
+following the tutorial on this video to create a webserver from scratch:
+https://www.youtube.com/watch?v=JhpUch6lWMw&t=108s
+
---
+++ index.js
@@ -0,0 +1,7 @@
+const app = require("express")()
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html `)
+})
+
+app.listen(8080, () => console.log('hi I\'m listening'))
